ccd9c16fd5a527254cb560c753e3d18705147613
Danny Roubin <dannyroubin1@gmail.com>
2021-10-12 15:17:45 -0700

    got fraction counter to work

--- Driver.java
+++ Driver.java
@@ -4,7 +4,20 @@ public class Driver {
         Fraction firstFraction = new Fraction();
         firstFraction.setNumerator(1);
         firstFraction.setDenominator(2);
+        Fraction secondFraction = new Fraction();
+        secondFraction.setNumerator(1);
+        secondFraction.setDenominator(2);
         System.out.println("Printing the first fraction: " + firstFraction.toString());
+
+
+        FractionCounter firstFractionFC = new FractionCounter(firstFraction);
+
+        if (firstFractionFC.compareAndIncrement(secondFraction) == true) {
+            System.out.println("yup it worked");
+        } else {
+            System.out.println("Nope");
+        }
+
     }
     
 }
--- FractionCounter.java
+++ FractionCounter.java
@@ -1,4 +1,29 @@
 // stores reference to a fraction object and counts how many duplicates it has
 public class FractionCounter {
     
+    Fraction theFraction;
+    int theCounter;
+
+    FractionCounter(Fraction tf) {
+        theFraction = tf;
+    }
+
+    boolean compareAndIncrement(Fraction newFraction) {
+        if (newFraction.getNumerator() == theFraction.getNumerator() && newFraction.getDenominator() == theFraction.getDenominator()) {
+            theCounter++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " theFraction='" + this.theFraction + "'" +
+            ", theCounter='" + this.theCounter + "'" +
+            "}";
+    }
+
 }
